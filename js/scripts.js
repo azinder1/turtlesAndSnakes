1d567ffe80aafe3020e268b7d9d2eed1724ca4bf
Adam Zinder and Matt Eichsteadt <adam@>
2016-10-20 11:57:47 -0700

    add quiz to page

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,25 @@ $(function() {
     $('#insects').show();
     $('#turtles').hide();
     $('#snakes').hide();
+  });
+
+  // ================================ Quiz Business Logic ======================
+  $('#quiz form').submit(function(event){
+    var favoriteThings = parseInt($("input:radio[name=favoriteThings]:checked").val());
+    var beverage = parseInt($("input:radio[name=beverage]:checked").val());
+    var actor = parseInt($("input:radio[name=actor]:checked").val());
+    var jetFuel = parseInt($("input:radio[name=jetFuel]:checked").val());
+    var dogsOut = parseInt($("input:radio[name=dogsOut]:checked").val());
+    var sum = favoriteThings + beverage + actor + jetFuel + dogsOut;
+
+    if(sum < 7 || beverage === 0) {
+      window.open()
+    } else if (sum >= 7) {
+      window.open()
+    } else {
+
+    }
+
+    event.preventDefault();
   })
 })
